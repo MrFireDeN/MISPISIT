1f07c1de2566f2549308824b887fc7710de27222
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-10-26 15:31:37 +0300

    feat: создан CPP_Roof

---
+++ Source/MISPISIT/Private/House/CPP_Roof.cpp
@@ -0,0 +1,11 @@
+// CPP_Roof.cpp
+
+#include "House/CPP_Roof.h"
+
+/**
+ * @file CPP_Roof.cpp
+ * 
+ * Implementation file for ACPP_Roof. Currently, this file only includes the header for ACPP_Roof,
+ * as all functionality is defined by the properties that represent the roof's dimensions.
+ * Additional behavior and functionality for ACPP_Roof can be implemented here as needed.
+ */
---
+++ Source/MISPISIT/Public/House/CPP_Roof.h
@@ -0,0 +1,37 @@
+// CPP_Roof.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "House/CPP_BuildingComponent.h"
+#include "CPP_Roof.generated.h"
+
+/**
+ * @class ACPP_Roof
+ * @brief Represents a roof component in a building with configurable dimensions.
+ * 
+ * Inherits from ACPP_BuildingComponent and provides properties for setting the roof's
+ * length and width, allowing for customizable roof sizes within the game environment.
+ */
+UCLASS()
+class MISPISIT_API ACPP_Roof : public ACPP_BuildingComponent
+{
+	GENERATED_BODY()
+	
+protected:
+	/**
+	 * @brief The length of the roof in units.
+	 * 
+	 * This property is editable in the editor and accessible in Blueprints for setting the roof's size.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Length;
+
+	/**
+	 * @brief The width of the roof in units.
+	 * 
+	 * Allows customization of the roof’s horizontal dimensions in the editor and access within Blueprints.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Width;
+};
