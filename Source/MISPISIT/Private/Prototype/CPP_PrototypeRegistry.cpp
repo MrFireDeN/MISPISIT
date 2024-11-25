82f075f344f685417b53c6f4267284672c015447
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-11-25 17:08:57 +0300

    feat: создан PrototypeRegistry

---
+++ Source/MISPISIT/Private/Prototype/CPP_PrototypeRegistry.cpp
@@ -0,0 +1,26 @@
+﻿// CPP_PrototypeRegistry.cpp
+
+#include "Prototype/CPP_PrototypeRegistry.h"
+
+ACPP_PrototypeRegistry::ACPP_PrototypeRegistry()
+{
+	// Create the SceneRoot component
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+
+	// Set SceneRoot as the RootComponent of this actor
+	RootComponent = SceneRoot;
+}
+
+// Called when the game starts or when spawned
+void ACPP_PrototypeRegistry::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ACPP_WallPrototype* BrickWallPrototype = GetWorld()->SpawnActor<ACPP_WallPrototype>(ACPP_WallPrototype::StaticClass(), FVector::Zero(), FRotator::ZeroRotator);
+	BrickWallPrototype->SetActorLabel("BrickWallPrototype");
+	BrickWallPrototype->SetStyle("Brick");
+	BrickWallPrototype->SetWallType(EWallType::ForWindow);
+
+	ACPP_Prototype* ConcreteBrickWall = BrickWallPrototype->Clone();
+	ConcreteBrickWall->SetActorLocation(this->GetActorLocation());
+}
---
+++ Source/MISPISIT/Public/Prototype/CPP_PrototypeRegistry.h
@@ -0,0 +1,24 @@
+﻿// CPP_PrototypeRegistry.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPP_WallPrototype.h"
+#include "CPP_PrototypeRegistry.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_PrototypeRegistry : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_PrototypeRegistry();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	// The default scene root component
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* SceneRoot;
+};
