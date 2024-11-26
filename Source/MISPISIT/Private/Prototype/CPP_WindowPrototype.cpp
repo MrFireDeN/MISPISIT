e434ac6f256020abb6e21584f14a354c393f4bed
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-11-26 16:52:14 +0300

    feat: реализован CPP_WindowPrototype

---
+++ Source/MISPISIT/Private/Prototype/CPP_WindowPrototype.cpp
@@ -0,0 +1,60 @@
+﻿// CPP_WindowPrototype.h
+
+#include "Prototype/CPP_WindowPrototype.h"
+
+ACPP_WindowPrototype::ACPP_WindowPrototype()
+{
+	WindowMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WindowMesh"));
+	RootComponent = WindowMesh;
+	
+	// WindowMeshAssetPaths
+	WindowMeshAssetPaths.Add(Styles[0], "/Game/Project/Models/House/Windows/SM_MedievalWindow.SM_MedievalWindow");
+	WindowMeshAssetPaths.Add(Styles[2], "/Game/Project/Models/House/Windows/SM_StoneWindow.SM_StoneWindow");
+	WindowMeshAssetPaths.Add(Styles[2], "/Game/Project/Models/House/Windows/SM_BrickWindow.SM_BrickWindow");
+	WindowMeshAssetPaths.Add(Styles[3], "/Game/Project/Models/House/Windows/SM_ModernWindow.SM_ModernWindow");
+
+	SetStyle(CurrentStyle);
+}
+
+void ACPP_WindowPrototype::CopyPropertiesTo(ACPP_Prototype*& Target)
+{
+	if (Target == nullptr)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Clone created from CPP_WallPrototype class"));
+		
+		Target =
+			Target = GetWorld()->SpawnActor<ACPP_WindowPrototype>(ACPP_WindowPrototype::StaticClass(), this->GetActorLocation(), this->GetActorRotation());
+	}
+
+	if (IsLightOn())
+	{
+		Cast<ACPP_WindowPrototype>(Target)->ToggleLight();
+	}
+	
+	Super::CopyPropertiesTo(Target);
+}
+
+void ACPP_WindowPrototype::ToggleLight()
+{
+	if (IsLightOn())
+	{
+		// TODO
+	}
+	else
+	{
+		// TODO
+	}
+}
+
+bool ACPP_WindowPrototype::IsLightOn()
+{
+	return false;
+}
+
+void ACPP_WindowPrototype::SetStyle(const FString Name)
+{
+	Super::SetStyle(Name);
+	
+	ACPP_BuildingComponent::LoadMeshFromAsset(WindowMesh, WindowMeshAssetPaths[CurrentStyle]);
+}
+
---
+++ Source/MISPISIT/Public/Prototype/CPP_WindowPrototype.h
@@ -0,0 +1,33 @@
+﻿// CPP_WindowPrototype.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_Prototype.h"
+#include "CPP_WindowPrototype.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_WindowPrototype : public ACPP_Prototype
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_WindowPrototype();
+
+protected:
+	virtual void CopyPropertiesTo(ACPP_Prototype*& Target) override;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* WindowMesh;
+
+	TMap<FString, FString> WindowMeshAssetPaths;
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	virtual void ToggleLight();
+
+	UFUNCTION(BlueprintCallable)
+	virtual bool IsLightOn();
+	
+	virtual void SetStyle(const FString Name) override;
+};
