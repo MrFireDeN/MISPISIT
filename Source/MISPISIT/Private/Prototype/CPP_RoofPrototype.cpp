96e96eaf26a11c505bb2c602dc1572f9c199b2b7
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-11-26 15:29:15 +0300

    feat: реализован CPP_RoofPrototype

---
+++ Source/MISPISIT/Private/Prototype/CPP_RoofPrototype.cpp
@@ -0,0 +1,42 @@
+﻿// CPP_RoofPrototype.cpp
+
+#include "Prototype/CPP_RoofPrototype.h"
+
+ACPP_RoofPrototype::ACPP_RoofPrototype()
+{
+	// Init mesh
+	RoofMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WindowMesh"));
+	RootComponent = RoofMesh;
+	
+	// RoofMeshAssetPaths
+	RoofMeshAssetPaths.Add(Styles[0], "/Game/Project/Models/House/Roof/SM_MedievalRoof.SM_MedievalRoof");
+	RoofMeshAssetPaths.Add(Styles[2], "/Game/Project/Models/House/Roof/SM_StoneRoof.SM_StoneRoof");
+	RoofMeshAssetPaths.Add(Styles[2], "/Game/Project/Models/House/Roof/SM_BrickRoof.SM_BrickRoof");
+	RoofMeshAssetPaths.Add(Styles[3], "/Game/Project/Models/House/Roof/SM_ModernRoof.SM_ModernRoof");
+
+	SetStyle(CurrentStyle);
+}
+
+void ACPP_RoofPrototype::CopyPropertiesTo(ACPP_Prototype*& Target)
+{
+	
+	if (Target == nullptr)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Clone created from CPP_WallPrototype class"));
+		
+		Target =
+			Target = GetWorld()->SpawnActor<ACPP_RoofPrototype>(ACPP_RoofPrototype::StaticClass(), this->GetActorLocation(), this->GetActorRotation());
+	}
+
+	Target->SetActorScale3D(this->GetActorScale3D());
+	
+	Super::CopyPropertiesTo(Target);
+}
+
+void ACPP_RoofPrototype::SetStyle(const FString Name)
+{
+	Super::SetStyle(Name);
+
+	ACPP_BuildingComponent::LoadMeshFromAsset(RoofMesh, RoofMeshAssetPaths[CurrentStyle]);
+}
+
---
+++ Source/MISPISIT/Public/Prototype/CPP_RoofPrototype.h
@@ -0,0 +1,27 @@
+﻿// CPP_RoofPrototype.cpp
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_Prototype.h"
+#include "CPP_RoofPrototype.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_RoofPrototype : public ACPP_Prototype
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_RoofPrototype();
+
+protected:
+	virtual void CopyPropertiesTo(ACPP_Prototype*& Target) override;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* RoofMesh;
+	
+	TMap<FString, FString> RoofMeshAssetPaths;
+
+public:
+	virtual void SetStyle(const FString Name) override;
+};
