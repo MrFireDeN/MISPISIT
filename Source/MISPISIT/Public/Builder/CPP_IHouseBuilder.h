23f8dec0331db421b8e5f6fbc536cc93de990c1b
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-12-05 18:23:25 +0300

    refactor: добавлены комментарии

--- Source/MISPISIT/Public/Builder/CPP_IHouseBuilder.h
+++ Source/MISPISIT/Public/Builder/CPP_IHouseBuilder.h
@@ -1,28 +1,60 @@
-﻿// CPP_IHouseBuilder.h
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
 #include "CPP_IHouseBuilder.generated.h"
 
-UINTERFACE()
-class UCPP_IHouseBuilder : public UInterface
+/**
+ * @brief Blueprintable interface for building houses. 
+ * Provides methods for setting walls, doors, windows, and roofs.
+ */
+UINTERFACE(Blueprintable, BlueprintType) 
+class MISPISIT_API UCPP_IHouseBuilder : public UInterface
 {
 	GENERATED_BODY()
 };
+
+/**
+ * @brief Interface class for implementing house building logic.
+ * Implement this interface in your classes to define how houses are built.
+ */
 class MISPISIT_API ICPP_IHouseBuilder
 {
 	GENERATED_BODY()
 
 public:
-	virtual void SetWalls(int Length, int Width) = 0;
+	/**
+	 * @brief Sets the dimensions of the walls.
+	 * @param Length The length of the walls.
+	 * @param Width The width of the walls.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "HouseBuilder")
+	bool SetWalls(int Length, int Width);
 
-	virtual void SetDoors(int Count) = 0;
+	/**
+	 * @brief Sets the number of doors.
+	 * @param Count The number of doors.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "HouseBuilder")
+	bool SetDoors(int Count);
 
-	virtual void SetWindows(int Count) = 0;
+	/**
+	 * @brief Sets the number of windows.
+	 * @param Count The number of windows.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "HouseBuilder")
+	bool SetWindows(int Count);
 
-	virtual void SetRoof(int Height) = 0;
+	/**
+	 * @brief Sets the height of the roof.
+	 * @param Height The height of the roof.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "HouseBuilder")
+	bool SetRoof(int Height);
 
-	virtual void Reset() = 0;
+	/**
+	 * @brief Resets the builder to its initial state.
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "HouseBuilder")
+	bool Reset();
 };
