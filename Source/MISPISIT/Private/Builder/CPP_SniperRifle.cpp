90c278cc7e00a7c677b4b3d70edc2ccb10b5a5bf
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-12-13 11:05:43 +0300

    feat: создан класс CPP_SniperRifle

---
+++ Source/MISPISIT/Private/Builder/CPP_SniperRifle.cpp
@@ -0,0 +1,7 @@
+﻿// CPP_SniperRifle.cpp
+
+#include "Builder/CPP_SniperRifle.h"
+
+ACPP_SniperRifle::ACPP_SniperRifle()
+{
+}
---
+++ Source/MISPISIT/Public/Builder/CPP_SniperRifle.h
@@ -0,0 +1,16 @@
+﻿// CPP_SniperRifle.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_Weapon.h"
+#include "CPP_SniperRifle.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_SniperRifle : public ACPP_Weapon
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_SniperRifle();
+};
