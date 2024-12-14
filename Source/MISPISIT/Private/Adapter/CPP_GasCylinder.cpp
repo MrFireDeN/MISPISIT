64420ba073b63cf965ce1740a037c71cb9c2f88b
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-12-14 11:30:02 +0300

    feat: создан CPP_GasCylinder

---
+++ Source/MISPISIT/Private/Adapter/CPP_GasCylinder.cpp
@@ -0,0 +1,69 @@
+﻿// CPP_GasCylinder.cpp
+
+#include "Adapter/CPP_GasCylinder.h"
+
+#include "WorldPartition/ContentBundle/ContentBundleLog.h"
+
+ACPP_GasCylinder::ACPP_GasCylinder()
+{
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	RootComponent = MeshComponent;
+
+	Volume = 1.0;
+	Mass = 5.0;
+	Modal = 0.028;
+}
+
+double ACPP_GasCylinder::GetPressure(const int& Temperature) const
+{
+	constexpr double R = 8.314;
+	return (Mass / Modal) * R * Temperature / Volume;
+}
+
+double ACPP_GasCylinder::AmountOfMatter() const
+{
+	return Mass / Modal;
+}
+
+FString ACPP_GasCylinder::ToString() const
+{
+	return FString::Printf(TEXT("Volume: %.2f m^3. Mass: %.2f kg. Modal: %.3f kg/mol"), Volume, Mass, Modal);
+}
+
+bool ACPP_GasCylinder::SetVolume(const double& NewVolume)
+{
+	if (NewVolume <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("You try to set Volume to %f which NOT possible"), NewVolume);
+		return false;
+	}
+
+	Volume = NewVolume;
+	SetActorScale3D(FVector(Volume));
+	return true;
+}
+
+bool ACPP_GasCylinder::SetMass(const double& NewMass)
+{
+	if (NewMass <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("You try to set Mass to %f which NOT possible"), NewMass);
+		return false;
+	}
+
+	Mass = NewMass;
+	return true;
+}
+
+bool ACPP_GasCylinder::SetModal(const double& NewModal)
+{
+	if (NewModal <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("You try to set Modal to %f which NOT possible"), NewModal);
+		return false;
+	}
+
+	Modal = NewModal;
+	return true;
+}
+
---
+++ Source/MISPISIT/Public/Adapter/CPP_GasCylinder.h
@@ -0,0 +1,48 @@
+﻿// CPP_GasCylinder.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPP_GasCylinder.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_GasCylinder : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_GasCylinder();
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* MeshComponent;
+	
+	UFUNCTION(BlueprintCallable, Category = "Gas")
+	virtual double GetPressure(const int& Temperature) const;
+	
+	UFUNCTION(BlueprintCallable, Category = "Gas")
+	virtual double AmountOfMatter() const;
+	
+	UFUNCTION(BlueprintCallable, Category = "Gas")
+	virtual FString ToString() const;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gas Properties")
+	double Volume; // m^3
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gas Properties")
+	double Mass; // kg
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gas Properties")
+	double Modal; // kg/mol
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Gas")
+	virtual bool SetVolume(const double& NewVolume);
+	
+	UFUNCTION(BlueprintCallable, Category = "Gas")
+	virtual bool SetMass(const double& NewMass);
+	
+	UFUNCTION(BlueprintCallable, Category = "Gas")
+	virtual bool SetModal(const double& NewModal);
+};
