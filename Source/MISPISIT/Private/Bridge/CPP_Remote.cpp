b7b85cbac43cf38ce0a0c59371d702a3cd810184
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-12-20 11:41:46 +0300

    feat: реализован пульт

---
+++ Source/MISPISIT/Private/Bridge/CPP_Remote.cpp
@@ -0,0 +1,108 @@
+﻿// CPP_Remote.cpp
+
+#include "Bridge/CPP_Remote.h"
+
+ACPP_Remote::ACPP_Remote()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RemoteMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RemoteMesh"));
+	RemoteMesh->SetupAttachment(RootComponent);
+	
+	UAssetLoader::LoadMeshFromAsset(RemoteMesh, "/Game/Project/Models/Bridge/TV_Pixel/SM_TV_Remote.SM_TV_Remote");
+}
+
+bool ACPP_Remote::Connect(ICPP_IDevice*& Target)
+{
+	if (!Target)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Target Device is NULL"));
+		return false;
+	}
+	
+	Device = Target;
+	
+	return true;
+}
+
+bool ACPP_Remote::TogglePower()
+{
+	if (!Device)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Device is NULL"));
+		return false;
+	}
+
+	return Device->IsEnabled() ? Device->Disable() : Device->Enable();
+}
+
+bool ACPP_Remote::VolumeDown()
+{
+	if (!Device)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Device is NULL"));
+		return false;
+	}
+
+	if (!Device->IsEnabled())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Can NOT VolumeDown. Device is DISABLED"));
+		return false;
+	}
+
+	const int NewVolume = FMath::Clamp(Device->GetVolume() - 10, 0, 100);
+	return Device->SetVolume(NewVolume);
+}
+
+bool ACPP_Remote::VolumeUp()
+{
+	if (!Device)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Device is NULL"));
+		return false;
+	}
+
+	if (!Device->IsEnabled())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Can NOT VolumeUp. Device is DISABLED"));
+		return false;
+	}
+
+	const int NewVolume = FMath::Clamp(Device->GetVolume() + 10, 0, 100);
+	return Device->SetVolume(NewVolume);
+}
+
+bool ACPP_Remote::ChannelDown()
+{
+	if (!Device)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Device is NULL"));
+		return false;
+	}
+
+	if (!Device->IsEnabled())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Can NOT ChannelDown. Device is DISABLED"));;
+		return false;
+	}
+
+	const int NewChannel = FMath::Clamp(Device->GetChannel() - 1, 0 , 3);
+	return Device->SetChannel(NewChannel);
+}
+
+bool ACPP_Remote::ChannelUp()
+{
+	if (!Device)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Device is NULL"));
+		return false;
+	}
+
+	if (!Device->IsEnabled())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Can NOT ChannelUp. Device is DISABLED"));
+		return false;
+	}
+
+	const int NewChannel = FMath::Clamp(Device->GetChannel() + 1, 0 , 3);
+	return Device->SetChannel(NewChannel);
+}
---
+++ Source/MISPISIT/Public/Bridge/CPP_Remote.h
@@ -0,0 +1,31 @@
+﻿// CPP_Remote.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AssetLoader.h"
+#include "CPP_IDevice.h"
+#include "CPP_Remote.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_Remote : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_Remote();
+
+protected:
+	UStaticMeshComponent* RemoteMesh;
+	ICPP_IDevice* Device;
+
+	virtual bool Connect(ICPP_IDevice*& Target);
+
+public:
+	virtual bool TogglePower();
+	virtual bool VolumeDown();
+	virtual bool VolumeUp();
+	virtual bool ChannelDown();
+	virtual bool ChannelUp();
+};
