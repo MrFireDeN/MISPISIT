527706de4e132761603def690e35be7968c7c5ea
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-12-22 16:36:32 +0300

    feat: реализовано радио

---
+++ Source/MISPISIT/Private/Bridge/CPP_Speaker.cpp
@@ -0,0 +1,116 @@
+﻿// CPP_Speaker.cpp
+
+#include "Bridge/CPP_Speaker.h"
+
+#include "Components/AudioComponent.h"
+
+ACPP_Speaker::ACPP_Speaker()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	SpeakerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SpeakerMesh"));
+	SpeakerBallMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SpeakerBallMesh"));
+
+	SpeakerMesh->SetupAttachment(RootComponent);
+	SpeakerBallMesh->SetupAttachment(SpeakerMesh);
+
+	SpeakerMesh->SetRelativeLocation(FVector(0, 0, 28));
+	SpeakerMesh->SetRelativeScale3D(FVector(0.5));
+
+	UAssetLoader::LoadMeshFromAsset(
+		SpeakerMesh,
+		"/Game/Project/Models/Bridge/Speaker/SM_Speaker.SM_Speaker"
+		);
+
+	UAssetLoader::LoadMeshFromAsset(
+		SpeakerBallMesh,
+		"/Game/Project/Models/Bridge/Speaker/SM_SpeakerSphere.SM_SpeakerSphere"
+	);
+
+	SpeakerAudio = CreateDefaultSubobject<UAudioComponent>(TEXT("SpeakerAudio"));
+	SpeakerAudio->SetupAttachment(RootComponent);
+}
+
+void ACPP_Speaker::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+bool ACPP_Speaker::IsEnabled()
+{
+	return !SpeakerBallMesh->GetRelativeRotation().Equals(*SpeakerBallRotations.Find("Disable"));
+}
+
+bool ACPP_Speaker::Enable()
+{
+	if (IsEnabled())
+	{
+		return true;
+	}
+
+	return SetChannel(CurrentChannel);
+}
+
+bool ACPP_Speaker::Disable()
+{
+	if (!IsEnabled())
+	{
+		return true;
+	}
+
+	SpeakerBallMesh->SetRelativeRotation(*SpeakerBallRotations.Find("Disable"));
+	SpeakerAudio->Stop();
+	return true;
+}
+
+int ACPP_Speaker::GetVolume()
+{
+	return CurrentVolume;
+}
+
+bool ACPP_Speaker::SetVolume(const int& NewVolume)
+{
+	CurrentVolume = FMath::Clamp(NewVolume, 0, 100);
+
+	SpeakerAudio->SetVolumeMultiplier(CurrentVolume / 100);
+	
+	return true;
+}
+
+int ACPP_Speaker::GetChannel()
+{
+	return CurrentChannel;
+}
+
+bool ACPP_Speaker::SetChannel(const int& NewChannel)
+{
+	CurrentChannel = FMath::Clamp(NewChannel, 0, 2);
+
+	SpeakerAudio->Stop();
+
+	switch (CurrentChannel)
+	{
+	case 0:
+		SpeakerBallMesh->SetRelativeRotation(*SpeakerBallRotations.Find("Channel1"));
+		SpeakerAudio->SetSound(SpeakerChannel1);
+		break;
+
+	case 1:
+		SpeakerBallMesh->SetRelativeRotation(*SpeakerBallRotations.Find("Channel2"));
+		SpeakerAudio->SetSound(SpeakerChannel2);
+		break;
+
+	case 2:
+		SpeakerBallMesh->SetRelativeRotation(*SpeakerBallRotations.Find("Channel3"));
+		SpeakerAudio->SetSound(SpeakerChannel3);
+		break;
+
+	default:
+		break;
+	}
+
+	SpeakerAudio->Play();
+	
+	return true;
+}
+
---
+++ Source/MISPISIT/Public/Bridge/CPP_Speaker.h
@@ -0,0 +1,71 @@
+﻿// CPP_Speaker.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AssetLoader.h"
+#include "CPP_IDevice.h"
+#include "CPP_Speaker.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_Speaker : public AActor, public ICPP_IDevice
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_Speaker();
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	USoundWave* SpeakerChannel1;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	USoundWave* SpeakerChannel2;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	USoundWave* SpeakerChannel3;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* SpeakerMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* SpeakerBallMesh;
+	
+	int CurrentChannel = 0;
+	float CurrentVolume = 50;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Audio")
+	UAudioComponent* SpeakerAudio;
+
+	const TMap<FString, FRotator> SpeakerBallRotations = {
+		{TEXT("Disable"), FRotator(0)},
+		{TEXT("Channel1"), FRotator(0, 90 ,0)},
+		{TEXT("Channel2"), FRotator(0, 180, 0)},
+		{TEXT("Channel3"), FRotator(0, 270, 0)},
+	};
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Speaker")
+	virtual bool IsEnabled() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Speaker")
+	virtual bool Enable() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Speaker")
+	virtual bool Disable() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Speaker")
+	virtual int GetVolume() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Speaker")
+	virtual bool SetVolume(const int& NewVolume) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Speaker")
+	virtual int GetChannel() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Speaker")
+	virtual bool SetChannel(const int& NewChannel) override;
+};
