dff0f87498dbc09f05ecc71e494d2e2251723ab5
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-02-11 17:29:19 +0300

    feat: создан сканнер

---
+++ Source/MISPISIT/Private/Compose/CPP_Scanner.cpp
@@ -0,0 +1,20 @@
+﻿// CPP_Scanner.cpp
+
+#include "Compose/CPP_Scanner.h"
+
+
+// Sets default values
+ACPP_Scanner::ACPP_Scanner()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	ScannerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ScannerMesh"));
+	RootComponent = ScannerMesh;
+	ScannerMesh->SetRelativeScale3D(FVector(0.5));
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetBoxExtent(FVector(50));
+	BoxCollision->SetupAttachment(RootComponent);
+	
+	UAssetLoader::LoadMeshFromAsset(ScannerMesh, "/Game/Project/Models/Compose/SM_Scanner.SM_Scanner");
+}
+
---
+++ Source/MISPISIT/Public/Compose/CPP_Scanner.h
@@ -0,0 +1,28 @@
+﻿// CPP_Scanner.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_Interactable.h"
+#include "GameFramework/Actor.h"
+#include "AssetLoader.h"
+#include "Components/BoxComponent.h"
+#include "CPP_Scanner.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_Scanner : public AActor, public ICPP_Interactable
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_Scanner();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* ScannerMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* BoxCollision;
+
+public:
+};
