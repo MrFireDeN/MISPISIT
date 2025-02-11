1b4264b68691522818a2c81faf4cc6b6e1d04c6d
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-02-11 19:34:44 +0300

    feat: реализован продукт Телефон

---
+++ Source/MISPISIT/Private/Compose/CPP_Phone.cpp
@@ -0,0 +1,51 @@
+﻿// CPP_Phone.cpp
+
+#include "Compose/CPP_Phone.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+ACPP_Phone::ACPP_Phone()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	PhoneMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PhoneMesh"));
+	PhoneMesh->SetupAttachment(RootComponent);
+	PhoneMesh->SetRelativeScale3D(FVector(0.1));
+	PhoneMesh->SetRelativeRotation(FRotator(0, 180, 0));
+	//PhoneMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetBoxExtent(FVector(50));
+	BoxCollision->SetupAttachment(RootComponent);
+	
+	UAssetLoader::LoadMeshFromAsset(PhoneMesh, "/Game/Project/Models/Compose/SM_Pixel_7a.SM_Pixel_7a");
+}
+
+float ACPP_Phone::GetPrice()
+{
+	return Price;
+}
+
+FName ACPP_Phone::GetName()
+{
+	return Name;
+}
+
+void ACPP_Phone::PlaceInBox()
+{
+	ICPP_IProduct::PlaceInBox();
+}
+
+bool ACPP_Phone::OnAttach()
+{
+	PhoneMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	return true;
+}
+
+bool ACPP_Phone::OnDetach()
+{
+	PhoneMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+	return true;
+}
+
---
+++ Source/MISPISIT/Public/Compose/CPP_Phone.h
@@ -0,0 +1,38 @@
+﻿// CPP_Phone.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_Interactable.h"
+#include "CPP_IProduct.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "AssetLoader.h"
+#include "CPP_Phone.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_Phone : public AActor, public ICPP_Interactable, public ICPP_IProduct
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_Phone();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* PhoneMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* BoxCollision;
+	
+	FName Name = "Google Pixel 7a";
+	float Price = 1000;
+	
+public:
+	virtual float GetPrice() override;
+	virtual FName GetName() override;
+	virtual void PlaceInBox() override;
+
+	virtual bool OnAttach() override;
+	virtual bool OnDetach() override;
+};
