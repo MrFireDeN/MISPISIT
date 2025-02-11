2b2a7c07f8a2ccbd7344292f594d283dfd03f39d
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-02-11 20:16:37 +0300

    feat: создана продуктовая коробка

---
+++ Source/MISPISIT/Private/Compose/CPP_ProductBox.cpp
@@ -0,0 +1,34 @@
+﻿// CPP_ProductBox.cpp
+
+#include "Compose/CPP_ProductBox.h"
+
+ACPP_ProductBox::ACPP_ProductBox()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	BoxMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PhoneMesh"));
+	BoxMesh->SetupAttachment(RootComponent);
+	BoxMesh->SetRelativeScale3D(FVector(0.25));
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetBoxExtent(FVector(50));
+	BoxCollision->SetupAttachment(RootComponent);
+	
+	UAssetLoader::LoadMeshFromAsset(BoxMesh, "/Script/Engine.StaticMesh'/Game/Project/Models/Compose/SM_PaperBox.SM_PaperBox'");
+}
+
+bool ACPP_ProductBox::OnAttach()
+{
+	BoxMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	BoxMesh->SetSimulatePhysics(false);
+	
+	return ICPP_Interactable::OnAttach();
+}
+
+bool ACPP_ProductBox::OnDetach()
+{
+	BoxMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	BoxMesh->SetSimulatePhysics(true);
+	
+	return ICPP_Interactable::OnDetach();
+}
+
---
+++ Source/MISPISIT/Public/Compose/CPP_ProductBox.h
@@ -0,0 +1,32 @@
+﻿// CPP_ProductBox.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AssetLoader.h"
+#include "CPP_Interactable.h"
+#include "CPP_IProduct.h"
+#include "Camera/CameraComponent.h"
+#include "Components/BoxComponent.h"
+#include "CPP_ProductBox.generated.h"
+
+UCLASS()
+class MISPISIT_API ACPP_ProductBox : public AActor, public ICPP_Interactable, public ICPP_IProduct
+{
+	GENERATED_BODY()
+
+public:
+	ACPP_ProductBox();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent*  ;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* BoxCollision;
+
+public:
+	virtual bool OnAttach() override;
+	virtual bool OnDetach() override;
+};
