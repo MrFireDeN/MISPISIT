af94aee393c8029c7de05b17a6261c79bcbe2b49
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-02-11 19:33:47 +0300

    feat: реализован интерфейс для продукта

---
+++ Source/MISPISIT/Public/Compose/CPP_IProduct.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "CPP_IProduct.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UCPP_IProduct : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MISPISIT_API ICPP_IProduct
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	virtual float GetPrice() { return 0; }
+	virtual FName GetName() { return "Name"; }
+	virtual void PlaceInBox() {}
+};
