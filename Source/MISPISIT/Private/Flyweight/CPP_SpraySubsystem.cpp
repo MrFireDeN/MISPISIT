5ce9ac5e875e58368c49682d6a595f24e3b901c4
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-04-10 19:22:00 +0300

    feat: создана субсистема для Легковеса

---
+++ Source/MISPISIT/Private/Flyweight/CPP_SpraySubsystem.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Flyweight/CPP_SpraySubsystem.h"
+
+const FCPP_SprayPattern* UCPP_SpraySubsystem::GetSprayPattern(const ESprayType Type) const
+{
+	if (Type == ESprayType::MAX)
+	{
+		return nullptr;
+	}
+
+	return SprayPatterns[static_cast<uint32>(Type)].Get();
+}
+
+void UCPP_SpraySubsystem::PostInitialize()
+{
+	Super::PostInitialize();
+	
+	SprayPatterns.Reserve(Count);
+
+	for (uint8 i = 0; i < Count; ++i)
+	{
+		ESprayType CurrentType = static_cast<ESprayType>(i);
+    
+		switch (CurrentType)
+		{
+		case ESprayType::AssaultRifle:
+			SprayPatterns.Add(MakeShared<FCPP_SprayPattern>(1));
+			UE_LOG(LogTemp, Log, TEXT("Processing Assault Rifle"));
+			break;
+		case ESprayType::Pistol:
+			SprayPatterns.Add(MakeShared<FCPP_SprayPattern>(2));
+			UE_LOG(LogTemp, Log, TEXT("Processing Pistol"));
+			break;
+		case ESprayType::Shotgun:
+			SprayPatterns.Add(MakeShared<FCPP_SprayPattern>(3));
+			UE_LOG(LogTemp, Log, TEXT("Processing Shotgun"));
+			break;
+		}
+	}
+}
---
+++ Source/MISPISIT/Public/Flyweight/CPP_SpraySubsystem.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_FSprayPattern.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "CPP_SpraySubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MISPISIT_API UCPP_SpraySubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	const FCPP_SprayPattern* GetSprayPattern(const ESprayType Type) const;
+
+protected:
+	virtual void PostInitialize() override;
+
+private:
+	uint8 Count = static_cast<uint8>(ESprayType::MAX);
+	
+	TArray<TSharedPtr<FCPP_SprayPattern>, TInlineAllocator<3>> SprayPatterns;
+};
