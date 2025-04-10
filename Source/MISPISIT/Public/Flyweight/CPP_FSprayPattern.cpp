2aa550f2fd862803483d152fe83c27ad123856e2
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-04-10 18:43:21 +0300

    feat: создана структура с массивными данными

---
+++ Source/MISPISIT/Public/Flyweight/CPP_FSprayPattern.cpp
@@ -0,0 +1,27 @@
+﻿#include "CPP_FSprayPattern.h"
+
+FCPP_SprayPattern::FCPP_SprayPattern(int32 InSeed, int32 Size)
+{
+	RandomStream.Initialize(InSeed);
+	SprayOffsets.Reserve(Size);
+	
+	for (int32 i = 0; i < Size; i++)
+	{
+		int32 X = RandomStream.RandRange(1, 3);
+		int32 Y = RandomStream.RandRange(-1, 1);
+		
+		SprayOffsets.Add(FVector2D(X, Y));
+	}
+}
+
+FVector2D FCPP_SprayPattern::GetSprayOffset(int i)
+{
+	if (i >= SprayOffsets.Num())
+	{
+		UE_LOG(LogTemp, Error, TEXT("Spray Offset is out of range"));
+		return FVector2D::ZeroVector;
+	}
+	
+	return SprayOffsets[i];
+}
+
---
+++ Source/MISPISIT/Public/Flyweight/CPP_FSprayPattern.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_FSprayPattern.generated.h"
+
+USTRUCT(BlueprintType)
+struct FCPP_SprayPattern
+{
+	GENERATED_BODY()
+
+	virtual ~FCPP_SprayPattern() = default;
+	FCPP_SprayPattern(int32 InSeed, int32 Size = 1024 * 1024);
+
+	UPROPERTY()
+	FRandomStream RandomStream;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Seed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FVector2D> SprayOffsets;
+
+	virtual FVector2D GetSprayOffset(int i);
+};
