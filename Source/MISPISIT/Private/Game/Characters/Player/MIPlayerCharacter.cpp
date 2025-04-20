b8062de263956860290c3dfa76ce8f9a775b82a6
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-04-20 22:30:56 +0300

    feat: настроен персонаж, однако...
    Нужно дописать таймер для камеры

--- Source/MISPISIT/Private/Game/Characters/Player/MIPlayerCharacter.cpp
+++ Source/MISPISIT/Private/Game/Characters/Player/MIPlayerCharacter.cpp
@@ -2,15 +2,104 @@
 
 #include "Game/Characters/Player/MIPlayerCharacter.h"
 
-AMIPlayerCharacter::AMIPlayerCharacter()
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AMIPlayerCharacter::AMIPlayerCharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	// CharacterMesh
+	GetMesh()->SetRelativeLocation(FVector(0, 0, -90));
+	GetMesh()->SetRelativeRotation(FRotator(0, 270, 0));
+
+	// SpringArm
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetCapsuleComponent());
+	SpringArm->SetRelativeLocation(FVector(0, 0, 72.f));
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->TargetArmLength = 100.f;
+
+	// Camera3P
+	ThirdPersonCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ThirdPersonCamera"));
+	ThirdPersonCamera->SetupAttachment(SpringArm);
+	ThirdPersonCamera->SetRelativeLocation(FVector(0, 50, 0));
+	ThirdPersonCamera->bUsePawnControlRotation = true;
+	ThirdPersonCamera->SetAutoActivate(false);
+	ThirdPersonCamera->SetActive(false);
+
+	// Camera1P
+	FirstPersonCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCamera->SetupAttachment(GetMesh(), TEXT("head"));
+	FirstPersonCamera->SetRelativeLocation(FVector(10, 12, 0));
+	FirstPersonCamera->SetRelativeRotation(FRotator(0, 90, 270));
+	FirstPersonCamera->bUsePawnControlRotation = true;
+	FirstPersonCamera->SetAutoActivate(false);
+	FirstPersonCamera->SetActive(false);
+
+	//CameraTransition
+	TransitionCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("TransitionCamera"));
+	TransitionCamera->SetupAttachment(GetCapsuleComponent());
+	TransitionCamera->bUsePawnControlRotation = true;
+	TransitionCamera->SetAutoActivate(false);
+	TransitionCamera->SetActive(false);
+
+	// Default Settings
+	bUseControllerRotationYaw = true;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+}
+
+void AMIPlayerCharacter::ChangeView()
+{
+	CurrentCamera->SetActive(false);
+
+	if (CurrentCamera == FirstPersonCamera)
+	{
+		CurrentCamera = ThirdPersonCamera;
+		bUseControllerRotationYaw = false;
+	}
+	else
+	{
+		CurrentCamera = FirstPersonCamera;
+		bUseControllerRotationYaw = true;
+	}
+
+	TransitionCamera->SetActive(true);
+	TransitionCamera->SetRelativeLocation(CurrentCamera->GetRelativeLocation());
+	bCameraTransiting = true;
+
+	// GetWorldTimerManager().SetTimer(CameraTransitionTimerHandle, this, )
 }
 
 void AMIPlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+}
+
+void AMIPlayerCharacter::PostInitProperties()
+{
+	Super::PostInitProperties();
+
+	if (!CurrentCamera)
+	{
+		CurrentCamera = FirstPersonCamera;
+	}
+}
+
+void AMIPlayerCharacter::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	if (CurrentCamera)
+	{
+		CurrentCamera->SetActive(true);
+	}
+}
+
+void AMIPlayerCharacter::UpdateCameraTransition()
+{
+	//
 }
 
 void AMIPlayerCharacter::Tick(float DeltaTime)
--- Source/MISPISIT/Public/Game/Characters/Player/MIPlayerCharacter.h
+++ Source/MISPISIT/Public/Game/Characters/Player/MIPlayerCharacter.h
@@ -6,18 +6,53 @@
 #include "../MICharacterBase.h"
 #include "MIPlayerCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+class USceneComponent;
+
 UCLASS()
 class MISPISIT_API AMIPlayerCharacter : public AMICharacterBase
 {
 	GENERATED_BODY()
 
 public:
-	AMIPlayerCharacter();
-	explicit AMIPlayerCharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+	explicit AMIPlayerCharacter(const FObjectInitializer& ObjectInitializer);
 	
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION(BlueprintCallable)
+	void ChangeView();
+
+	UFUNCTION(BlueprintPure)
+	UCameraComponent* GetCurrentCamera() const { return CurrentCamera; }
+
 protected:
 	virtual void BeginPlay() override;
+	virtual void PostInitProperties() override;
+	virtual void PostInitializeComponents() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	TObjectPtr<UCameraComponent> ThirdPersonCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	TObjectPtr<UCameraComponent> FirstPersonCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	TObjectPtr<UCameraComponent> TransitionCamera;
+
+	UPROPERTY(EditDefaultsOnly, Category = Camera)
+	float CameraTransitionSpeed = 5.f;
+
+private:
+	TObjectPtr<UCameraComponent> CurrentCamera;
+
+	FTimerHandle CameraTransitionTimerHandle;
+
+	bool bCameraTransiting = false;
+
+	void UpdateCameraTransition();
 };
