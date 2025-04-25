bb0cf9796db4de8fac2318742a763c2aa6179f19
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-04-25 10:32:17 +0300

    feat: создан компонент здоровья

---
+++ Source/MISPISIT/Private/Game/Gameplay/Components/MIHealthComponent.cpp
@@ -0,0 +1,50 @@
+﻿// MIHealthComponent.cpp
+
+#include "Game/Gameplay/Components/MIHealthComponent.h"
+
+
+UMIHealthComponent::UMIHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UMIHealthComponent::TakeDamage(const float Damage)
+{
+	if (IsDead() || Damage <= 0) return;
+	
+	const float NewHealth = FMath::Clamp(CurrentHealth - Damage, 0.f, MaxHealth);
+	UpdateHealth(NewHealth);
+	
+	UE_LOG(LogTemp, Display, TEXT("Take Damage"));
+}
+
+void UMIHealthComponent::Heal(const float Amount)
+{
+	if (IsDead() || Amount <= 0) return;
+	
+	const float NewHealth = FMath::Clamp(CurrentHealth + Amount, 0.f, MaxHealth);
+	UpdateHealth(NewHealth);
+	
+	UE_LOG(LogTemp, Display, TEXT("Heal"));
+}
+
+void UMIHealthComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+	CurrentHealth = MaxHealth;
+}
+
+float UMIHealthComponent::UpdateHealth(const float NewHealth)
+{
+	const bool bHealthChanged = NewHealth != CurrentHealth;
+	CurrentHealth = NewHealth;
+
+	if (bHealthChanged)
+	{
+		OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
+		if (IsDead()) OnDeath.Broadcast();
+	}
+
+	return CurrentHealth;
+}
+
---
+++ Source/MISPISIT/Public/Game/Gameplay/Components/MIHealthComponent.h
@@ -0,0 +1,51 @@
+﻿// MIHealthComponent.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MIHealthComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHealthChangedSignature, float, CurrentHealth, float, MaxHealth);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class MISPISIT_API UMIHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UMIHealthComponent();
+
+	UPROPERTY(BlueprintAssignable, Category="Events")
+	FOnDeathSignature OnDeath;
+
+	UPROPERTY(BlueprintAssignable, Category="Events")
+	FOnHealthChangedSignature OnHealthChanged;
+
+	UFUNCTION(BlueprintCallable, Category="Health")
+	virtual void TakeDamage(const float Damage);
+
+	UFUNCTION(BlueprintCallable, Category="Health")
+	virtual void Heal(const float Amount);
+
+	UFUNCTION(BlueprintPure, Category="Health")
+	virtual bool IsDead() const { return CurrentHealth <= KINDA_SMALL_NUMBER; };
+
+	UFUNCTION(BlueprintPure, Category="Health")
+	virtual float GeCurrentHealth() const { return CurrentHealth; }
+
+	UFUNCTION(BlueprintPure, Category="Health")
+	virtual float GetMaxHealth() const { return MaxHealth; }
+
+protected:
+	virtual void InitializeComponent() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Health")
+	float MaxHealth = 100;
+	
+	UPROPERTY(VisibleInstanceOnly, Category = "Health")
+	float CurrentHealth;
+
+	float UpdateHealth(const float NewHealth);
+};
