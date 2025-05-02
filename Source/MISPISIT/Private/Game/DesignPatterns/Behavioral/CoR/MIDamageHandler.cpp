fc02fff8a23159866c3652af8c30a748f0835d23
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-02 14:03:09 +0300

    feat: создан DamageHandler

---
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/CoR/MIDamageHandler.cpp
@@ -0,0 +1,46 @@
+﻿// MIDamageHandler.cpp
+
+#include "Game/DesignPatterns/Behavioral/CoR/MIDamageHandler.h"
+#include "GameFramework/DamageType.h"
+
+UMIDamageHandlerBase::UMIDamageHandlerBase() = default;
+
+void UMIDamageHandlerBase::HandleDamage(float DamageAmount, const FDamageEvent& DamageEvent)
+{
+	if (CanHandle(DamageAmount, DamageEvent))
+	{
+		const float DamageProcessed = ApplyEffects(DamageAmount, DamageEvent);
+		const float RemainingDamage = DamageAmount - DamageProcessed;
+
+		if (RemainingDamage > KINDA_SMALL_NUMBER && NextHandler)
+		{
+			NextHandler->HandleDamage(RemainingDamage, DamageEvent);
+		}
+	}
+	else if (NextHandler)
+	{
+		NextHandler->HandleDamage(DamageAmount, DamageEvent);
+	}
+}
+
+void UMIDamageHandlerBase::SetNextHandler(const TScriptInterface<IMIDamageHandler>& Next)
+{
+	if (!Next)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[%s] SetNextHandler: NextHandler is NULL"), *GetClass()->GetName());
+		return;
+	}
+
+	NextHandler = Next;
+}
+
+bool UMIDamageHandlerBase::CanHandle(float DamageAmount, const FDamageEvent& DamageEvent)
+{
+	return DamageAmount > KINDA_SMALL_NUMBER;
+}
+
+float UMIDamageHandlerBase::ApplyEffects(float DamageAmount, const FDamageEvent& DamageEvent)
+{
+	// Default implementation applies nothing.
+	return 0.f;
+}
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/CoR/MIDamageHandler.h
@@ -0,0 +1,68 @@
+﻿// MIDamageHandler.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MIDamageHandler.generated.h"
+
+/**
+ * Interface for implementing a Chain of Responsibility for handling damage.
+ * Each handler can choose to apply part of the damage and pass the rest to the next handler.
+ */
+UINTERFACE(BlueprintType)
+class MISPISIT_API UMIDamageHandler : public UInterface
+{
+  GENERATED_BODY()
+};
+
+class MISPISIT_API IMIDamageHandler
+{
+  GENERATED_BODY()
+
+public:
+  virtual void HandleDamage(float DamageAmount, const FDamageEvent& DamageEvent) = 0;
+  virtual void SetNextHandler(const TScriptInterface<IMIDamageHandler>& Next) = 0;
+};
+
+/**
+ * Base UObject implementation of a Damage Handler in a Chain of Responsibility.
+ */
+UCLASS(Abstract, Blueprintable)
+class MISPISIT_API UMIDamageHandlerBase : public UObject, public IMIDamageHandler
+{
+  GENERATED_BODY()
+
+public:
+  UMIDamageHandlerBase();
+
+  /**
+   * Handles the incoming damage. Implementations may apply part of it and forward the rest.
+   * @param DamageAmount - The total amount of incoming damage.
+   * @param DamageEvent - Struct containing details about the damage.
+   */
+  virtual void HandleDamage(float DamageAmount, const FDamageEvent& DamageEvent) override;
+
+  /**
+   * Sets the next handler in the chain.
+   * @param Next - The next damage handler to call if this one doesn't fully consume the damage.
+   */
+  virtual void SetNextHandler(const TScriptInterface<IMIDamageHandler>& Next) override;
+
+protected:
+  /** Pointer to the next handler in the chain */
+  UPROPERTY()
+  TScriptInterface<IMIDamageHandler> NextHandler;
+
+  /**
+   * Determines if this handler can process the given damage.
+   * @return true if this handler can process it.
+   */
+  virtual bool CanHandle(float DamageAmount, const FDamageEvent& DamageEvent);
+
+  /**
+   * Applies the specific effects of this damage handler.
+   * @return The amount of damage this handler has processed.
+   */
+  virtual float ApplyEffects(float DamageAmount, const FDamageEvent& DamageEvent);
+};
