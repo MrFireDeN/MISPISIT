94a2ea04b7fe85cb9aa93451ef77719cd5779bec
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-06 15:47:46 +0300

    feat: созданы интерфейсы для стратегии

---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Strategy/MIDamageStrategy.h
@@ -0,0 +1,33 @@
+﻿// MIDamageStrategy.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MIDamageStrategy.generated.h"
+
+UINTERFACE(BlueprintType)
+class MISPISIT_API UMIDamageStrategy : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Interface for defining how damage is modified or applied
+ * based on the type of damage, resistances, vulnerabilities, etc.
+ */
+class MISPISIT_API IMIDamageStrategy
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Calculates modified damage based on damage type and modifiers.
+	 * @param DamageAmount - Base damage value.
+	 * @param DamageEvent - Context of the incoming damage.
+	 * @return Final damage after applying resistances or amplifications.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Damage")
+	float ApplyDamage(float DamageAmount, const FDamageEvent& DamageEvent);
+};
+
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Strategy/MIDamageStrategyProvider.h
@@ -0,0 +1,32 @@
+﻿// MIDamageStrategyProvider.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MIDamageStrategyProvider.generated.h"
+
+class IMIDamageStrategy;
+
+/**
+ * Interface for providing one or more damage strategies (damage "perks")
+ * to modify or react to incoming damage. Implemented by components like Health, Shield, or Armor.
+ */
+UINTERFACE(BlueprintType, MinimalAPI)
+class UMIDamageStrategyProvider : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class MISPISIT_API IMIDamageStrategyProvider
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Returns an array of strategy objects (perks) that influence how damage is modified.
+	 * Each strategy can adjust the final damage based on custom logic like resistances, vulnerabilities, or passive effects.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Damage")
+	TArray<TScriptInterface<IMIDamageStrategy>> GetDamageStrategies() const;
+};
