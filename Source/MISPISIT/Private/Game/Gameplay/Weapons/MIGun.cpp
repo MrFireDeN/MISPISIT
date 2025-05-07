87d6831ddf5cfeb6ae5844c52cf87177989cedee
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-07 19:51:21 +0300

    feat: создан новый класс для оружия

---
+++ Source/MISPISIT/Private/Game/Gameplay/Weapons/MIGun.cpp
@@ -0,0 +1,86 @@
+﻿// MIGun.cpp
+
+
+#include "Game/Gameplay/Weapons/MIGun.h"
+#include "AssetLoader.h"
+#include "Game/Characters/MICharacterBase.h"
+#include "Game/DesignPatterns/Behavioral/State/MIGunState.h"
+#include "Game/DesignPatterns/Behavioral/State/MIGunState_Idle.h"
+
+
+// Sets default values
+AMIGun::AMIGun()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Mesh->SetupAttachment(RootComponent);
+}
+
+bool AMIGun::OnAttach()
+{
+	CharacterOwner = Cast<AMICharacterBase>(GetAttachParentActor());
+
+	if (!CharacterOwner)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun attached to non-character actor"));
+		return false;
+	}
+	
+	return true;
+}
+
+bool AMIGun::OnDetach()
+{
+	CharacterOwner = nullptr;
+	return true;
+}
+
+bool AMIGun::OnPrimaryAction()
+{
+	StartFire();
+	
+	return ICPP_Interactable::OnPrimaryAction();
+}
+
+void AMIGun::StartFire()
+{
+	if (CurrentState != nullptr)
+	{
+		CurrentState->Execute_StartFire(CurrentState.GetObject());
+	}
+}
+
+void AMIGun::StopFire()
+{
+	if (CurrentState != nullptr)
+	{
+		CurrentState->Execute_StopFire(CurrentState.GetObject());
+	}
+}
+
+void AMIGun::Reload()
+{
+	if (CurrentState != nullptr)
+	{
+		CurrentState->Execute_Reload(CurrentState.GetObject());
+	}
+}
+
+void AMIGun::SetState(TScriptInterface<IMIGunState> NewState)
+{
+	if (!NewState)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[%s] SetState: Invalid Gun State"));
+		return;
+	}
+	
+	CurrentState = NewState;
+}
+
+// Called when the game starts or when spawned
+void AMIGun::BeginPlay()
+{
+	Super::BeginPlay();
+	SetState(NewObject<UMIGunState_Idle>(this));
+}
---
+++ Source/MISPISIT/Public/Game/Gameplay/Weapons/MIGun.h
@@ -0,0 +1,58 @@
+﻿// MIGun.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_Interactable.h"
+#include "GameFramework/Actor.h"
+#include "MIGun.generated.h"
+
+class UStaticMeshComponent;
+class IMIGunState;
+
+UENUM(BlueprintType)
+enum EFireMode : uint8
+{
+	Single,
+	Auto,
+	Burst
+};
+
+UCLASS(Abstract, BlueprintType)
+class MISPISIT_API AMIGun : public AActor, public ICPP_Interactable
+{
+	GENERATED_BODY()
+
+public:
+	AMIGun();
+
+	virtual bool OnAttach() override;
+	virtual bool OnDetach() override;
+	virtual bool OnPrimaryAction() override;
+
+	virtual void StartFire();
+	virtual void StopFire();
+	virtual void Reload();
+
+	void SetState(TScriptInterface<IMIGunState> NewState);
+
+	UFUNCTION(BlueprintPure, Category = "Gun")
+	TScriptInterface<IMIGunState> GetCurrentState() const { return CurrentState; }
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"), Category = "Gun")
+	UStaticMeshComponent* Mesh;
+	
+	/** Character currently wielding this weapon */
+	UPROPERTY(BlueprintReadOnly)
+	ACharacter* CharacterOwner;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Gun")
+	uint8 FireMode = EFireMode::Single;
+
+private:
+	UPROPERTY()
+	TScriptInterface<IMIGunState> CurrentState;
+};
