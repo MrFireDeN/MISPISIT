e8d9208f9e5655f22d3878cd74b20641551694ef
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-13 16:31:17 +0300

    feat: создан пуль для управления Командами

---
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Command/MICommandRemote.cpp
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/DesignPatterns/Behavioral/Command/MICommandRemote.h"
+
+#include "TraceHelper.h"
+#include "Game/Characters/MICharacterBase.h"
+#include "Game/DesignPatterns/Behavioral/Command/MIAIController_Command.h"
+#include "Game/DesignPatterns/Behavioral/Command/MICommand.h"
+#include "Game/DesignPatterns/Behavioral/Command/MILookAtCommand.h"
+#include "Game/DesignPatterns/Behavioral/Command/MIMoveToCommand.h"
+
+
+// Sets default values
+AMICommandRemote::AMICommandRemote()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+bool AMICommandRemote::OnPrimaryAction()
+{
+	if (!TargetController.Get()) return false;
+	
+	TOptional<FHitResult> HitResult = GetTraceHelper()->TraceFromPlayer();
+
+	if (!HitResult) return false;
+	
+	const FHitResult& Hit = *HitResult;
+
+	if (!Hit.IsValidBlockingHit()) return false;
+
+	TScriptInterface<IMICommand> Command = NewObject<UMIMoveToCommand>();
+	Cast<UMIMoveToCommand>(Command.GetObject())->Initialize(TargetController, Hit.Location);
+	Command->Execute_Execute(Command.GetObject());
+
+	return true;
+}
+
+bool AMICommandRemote::OnSecondaryAction()
+{
+	if (!TargetController.Get()) return false;
+	
+	TOptional<FHitResult> HitResult = GetTraceHelper()->TraceFromPlayer();
+
+	if (!HitResult) return false;
+	
+	const FHitResult& Hit = *HitResult;
+
+	if (!Hit.IsValidBlockingHit()) return false;
+	
+	AActor* HitActor = Hit.GetActor();
+	
+	if (!HitActor) return false;
+
+	TScriptInterface<IMICommand> Command = NewObject<UMILookAtCommand>();
+	Cast<UMILookAtCommand>(Command.GetObject())->Initialize(TargetController, HitActor);
+	Command->Execute_Execute(Command.GetObject());
+
+	return true;
+}
+
+bool AMICommandRemote::OnNumericAction(const int Digit)
+{
+	return ICPP_Interactable::OnNumericAction(Digit);
+}
+
+void AMICommandRemote::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (Target.Get()) {
+		TargetController = Cast<AMIAIController_Command>(Target->GetController());
+	}
+	
+}
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Command/MICommandRemote.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/Gameplay/MIInteractableBase.h"
+#include "MICommandRemote.generated.h"
+
+class AMIAIController_Command;
+class AMICharacterBase;
+
+UCLASS()
+class MISPISIT_API AMICommandRemote : public AMIInteractableBase
+{
+	GENERATED_BODY()
+
+public:
+	AMICommandRemote();
+
+	virtual bool OnPrimaryAction() override;
+	virtual bool OnSecondaryAction() override;
+	virtual bool OnNumericAction(const int Digit) override;
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Remote", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<AMICharacterBase> Target = nullptr;
+
+private:
+	UPROPERTY()
+	TObjectPtr<AMIAIController_Command> TargetController;
+};
