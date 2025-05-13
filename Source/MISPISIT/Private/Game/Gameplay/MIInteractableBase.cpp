f59f2b24371a800627ac48cf3518292435dfea86
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-13 16:30:52 +0300

    feat: создан InteractableBase

---
+++ Source/MISPISIT/Private/Game/Gameplay/MIInteractableBase.cpp
@@ -0,0 +1,60 @@
+﻿// MIInteractableBase.cpp
+
+
+#include "Game/Gameplay/MIInteractableBase.h"
+
+#include "Components/BoxComponent.h"
+#include "Game/Characters/MICharacterBase.h"
+#include "TraceHelper.h"
+
+
+AMIInteractableBase::AMIInteractableBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Mesh->SetupAttachment(RootComponent);
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetBoxExtent(FVector(50));
+	BoxCollision->SetupAttachment(RootComponent);
+}
+
+bool AMIInteractableBase::OnAttach()
+{
+	CharacterOwner = Cast<AMICharacterBase>(GetAttachParentActor());
+
+	if (!CharacterOwner)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun attached to non-character actor"));
+		return false;
+	}
+	
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+	return true;
+}
+
+bool AMIInteractableBase::OnDetach()
+{
+	CharacterOwner = nullptr;
+	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	
+	return true;
+}
+
+void AMIInteractableBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Initialize trace helper subsystem
+	TraceHelper = GetWorld()->GetSubsystem<UTraceHelper>();
+	if (!TraceHelper)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red,
+			TEXT("Failed to load TraceHelper subsystem"));
+	}
+}
+
---
+++ Source/MISPISIT/Public/Game/Gameplay/MIInteractableBase.h
@@ -0,0 +1,47 @@
+﻿// MIInteractableBase.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_Interactable.h"
+#include "GameFramework/Actor.h"
+#include "MIInteractableBase.generated.h"
+
+class AMICharacterBase;
+class UStaticMeshComponent;
+class UBoxComponent;
+class UTraceHelper;
+
+UCLASS(BlueprintType, Blueprintable)
+class MISPISIT_API AMIInteractableBase : public AActor, public ICPP_Interactable
+{
+	GENERATED_BODY()
+
+public:
+	AMIInteractableBase();
+
+	virtual bool OnAttach() override;
+	virtual bool OnDetach() override;
+
+	UFUNCTION(BlueprintPure, Category = "Interactable")
+	AMICharacterBase* GetCharacterOwner() const { return CharacterOwner; }
+
+	UFUNCTION(BlueprintPure, Category = "Interactable")
+	UTraceHelper* GetTraceHelper() { return TraceHelper; }
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"), Category = "Interactable")
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"), Category = "Interactable")
+	TObjectPtr<UBoxComponent> BoxCollision;
+
+private:
+	UPROPERTY()
+	TObjectPtr<AMICharacterBase> CharacterOwner = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UTraceHelper> TraceHelper = nullptr;
+};
