729b307f09f6387339566fb89acdaafad16a2db9
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-14 20:27:21 +0300

    feat: создан TimerServerSubsystem

---
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Observer/MITimeServerSubsystem.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/DesignPatterns/Behavioral/Observer/MITimeServerSubsystem.h"
+
+#include "Game/DesignPatterns/Behavioral/Observer/MIObserver.h"
+
+void UMITimeServerSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	GetWorld()->GetTimerManager().SetTimer(
+		TimerHandle,
+		this,
+		&UMITimeServerSubsystem::NotifyObservers,
+		1.f,
+		true
+		);
+}
+
+void UMITimeServerSubsystem::Deinitialize()
+{
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+	Observers.Empty();
+	
+	Super::Deinitialize();
+}
+
+void UMITimeServerSubsystem::Subscribe(UObject* Observer)
+{
+	Observers.Add(Observer);
+}
+
+void UMITimeServerSubsystem::Unsubscribe(UObject* Observer)
+{
+	Observers.Remove(Observer);
+}
+
+void UMITimeServerSubsystem::NotifyObservers()
+{
+	TimeState++;
+	
+	for (auto& ObserverPtr : Observers)
+	{
+		if (UObject* Observer = ObserverPtr.Get())
+		{
+			if (Observer->GetClass()->ImplementsInterface(UMIObserver::StaticClass()))
+			{
+				IMIObserver::Execute_OnTimeUpdate(Observer, TimeState);
+			}
+		}
+	}
+
+	UE_LOG(LogTemp, Display, TEXT("[%s] Tick: %d"), *GetNameSafe(this), TimeState);
+}
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Observer/MITimeServerSubsystem.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "MITimeServerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MISPISIT_API UMITimeServerSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Observer")
+	void Subscribe(UObject* Observer);
+
+	UFUNCTION(BlueprintCallable, Category = "Observer")
+	void Unsubscribe(UObject* Observer);
+
+private:
+	UPROPERTY()
+	int32 TimeState = 0;
+
+	UPROPERTY()
+	FTimerHandle TimerHandle;
+
+	UPROPERTY()
+	TArray<TWeakObjectPtr<UObject>> Observers;
+
+	void NotifyObservers();
+};
