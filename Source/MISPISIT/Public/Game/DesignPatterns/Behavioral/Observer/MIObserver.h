3be34b81732470d85e85437e8d5f647680b3ce0a
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-14 20:26:19 +0300

    feat: создан интерфейс для наблюдателей

---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Observer/MIObserver.h
@@ -0,0 +1,27 @@
+﻿// MIObserver.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MIObserver.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UMIObserver : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MISPISIT_API IMIObserver
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Observer")
+	void OnTimeUpdate(int32 NewTime);
+};
