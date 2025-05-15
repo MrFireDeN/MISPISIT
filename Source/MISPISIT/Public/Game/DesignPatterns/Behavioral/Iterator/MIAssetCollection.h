871e00e9bd99a2e420b43b372af955e6eb02ad3f
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-15 22:32:55 +0300

    feat: созданы интерфейсы для итератора

---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Iterator/MIAssetCollection.h
@@ -0,0 +1,27 @@
+﻿// MIAssetCollection.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MIAssetCollection.generated.h"
+
+class IMIAssetIterator;
+
+UINTERFACE()
+class UMIAssetCollection : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MISPISIT_API IMIAssetCollection
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Iterator")
+	TScriptInterface<IMIAssetIterator> CreateIterator();
+};
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Iterator/MIAssetIterator.h
@@ -0,0 +1,37 @@
+﻿// MIAssetIterator.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MIAssetIterator.generated.h"
+
+UINTERFACE(Blueprintable, BlueprintType)
+class UMIAssetIterator : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MISPISIT_API IMIAssetIterator
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Iterator")
+	UObject* GetNext();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Iterator")
+	UObject* GetPrevious();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Iterator")
+	bool HasNext();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Iterator")
+	bool HasPrevious();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Iterator")
+	void Reset();
+};
