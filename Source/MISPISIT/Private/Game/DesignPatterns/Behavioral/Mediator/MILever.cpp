37729656483b53fb110f9c0580216f384787c568
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-21 11:59:21 +0300

    feat: создан рычаг

---
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Mediator/MILever.cpp
@@ -0,0 +1,54 @@
+﻿// MILever.cpp
+
+#include "Game/DesignPatterns/Behavioral/Mediator/MILever.h"
+
+
+AMILever::AMILever()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultRoot"));
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+	ID = FName("Lever");
+}
+
+void AMILever::Toggle()
+{
+	UE_LOG(LogTemp, Error, TEXT("[%s]: Toggle"), *GetNameSafe(this));
+	
+	bIsOn = !bIsOn;
+
+	OnLeverToggled(bIsOn);
+
+	Execute_NotifyMediator(this);
+}
+
+void AMILever::ReceiveEvent_Implementation(FName EventName)
+{
+	if (EventName == FName("Reset"))
+	{
+		bIsOn = false;
+		OnLeverToggled(bIsOn);
+		return;
+	}
+
+	if (EventName == FName("ForceOn"))
+	{
+		bIsOn = true;
+		OnLeverToggled(bIsOn);
+		return;
+	}
+
+	if (EventName == FName("ForceOff"))
+	{
+		bIsOn = false;
+		OnLeverToggled(bIsOn);
+		return;
+	}
+
+	UE_LOG(LogTemp, Error, TEXT("[%s]: EventName(%s) is NOT valid"), *GetNameSafe(this), *EventName.ToString());
+}
+
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Mediator/MILever.h
@@ -0,0 +1,33 @@
+﻿// MILever.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MIPuzzleElementBase.h"
+#include "MILever.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS(Blueprintable, BlueprintType)
+class MISPISIT_API AMILever : public AMIPuzzleElementBase
+{
+	GENERATED_BODY()
+
+public:
+	AMILever();
+
+	UFUNCTION(BlueprintCallable, Category = "Lever")
+	void Toggle();
+
+	virtual void ReceiveEvent_Implementation(FName EventName) override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lever")
+	bool bIsOn = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Lever", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Lever")
+	void OnLeverToggled(bool bNewState);
+};
