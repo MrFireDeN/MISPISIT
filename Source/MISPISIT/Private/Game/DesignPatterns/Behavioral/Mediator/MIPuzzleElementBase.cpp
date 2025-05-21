e4f3dd50e2c3dc53a7d4a768475a5c0dd5dde02e
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-21 11:08:53 +0300

    feat: создан базовый актер для интерфейса

---
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleElementBase.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/DesignPatterns/Behavioral/Mediator/MIPuzzleElementBase.h"
+
+#include "Game/DesignPatterns/Behavioral/Mediator/MIPuzzleMediator.h"
+
+void AMIPuzzleElementBase::RegisterWithMediator_Implementation(UMIPuzzleMediator* InMediator)
+{
+	if (InMediator == nullptr) return;
+
+	Mediator = InMediator;
+	Mediator->RegisterElement(ID, this);
+}
+
+void AMIPuzzleElementBase::NotifyMediator_Implementation()
+{
+	if (IsValid(Mediator.Get()))
+	{
+		Mediator->NotifyFrom(ID);
+	}
+}
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleElementBase.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MIPuzzleElement.h"
+#include "GameFramework/Actor.h"
+#include "MIPuzzleElementBase.generated.h"
+
+class UMIPuzzleMediator;
+
+UCLASS(Abstract)
+class MISPISIT_API AMIPuzzleElementBase : public AActor, public IMIPuzzleElement
+{
+	GENERATED_BODY()
+
+public:
+	virtual void RegisterWithMediator_Implementation(UMIPuzzleMediator* InMediator) override;
+	virtual void NotifyMediator_Implementation() override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mediator")
+	TObjectPtr<UMIPuzzleMediator> Mediator;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mediator", meta = (AllowPrivateAccess = "true"))
+	FName ID;
+};
