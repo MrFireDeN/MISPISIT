f2f385609f4325da446b1417bb1d4e43125340e3
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-22 15:06:33 +0300

    feat: создана кнопка для медиатора

---
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleButton.cpp
@@ -0,0 +1,52 @@
+﻿// MIPuzzleButton.cpp
+
+
+#include "Game/DesignPatterns/Behavioral/Mediator/MIPuzzleButton.h"
+
+#include "Game/DesignPatterns/Behavioral/Mediator/MIPuzzleMediator.h"
+
+
+AMIPuzzleButton::AMIPuzzleButton()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+	ID = FName("Button");
+}
+
+void AMIPuzzleButton::Press()
+{
+	if (bIsActive) return;
+
+	bIsActive = true;
+	OnPressed();
+	Execute_NotifyMediator(this);
+
+	GetWorld()->GetTimerManager().SetTimer(
+		ButtonTimerHandle,
+		this,
+		&AMIPuzzleButton::Expire,
+		ActiveDuration,
+		false
+		);
+}
+
+void AMIPuzzleButton::ReceiveEvent_Implementation(FName EventName)
+{
+	if (EventName == "Reset")
+	{
+		bIsActive = false;
+		GetWorld()->GetTimerManager().ClearTimer(ButtonTimerHandle);
+		OnExpired();
+	}
+}
+
+void AMIPuzzleButton::Expire()
+{
+	bIsActive = false;
+	OnExpired();
+	Execute_NotifyMediator(this);
+}
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleButton.h
@@ -0,0 +1,46 @@
+﻿// MIPuzzleButton.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MIPuzzleElementBase.h"
+#include "MIPuzzleButton.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class MISPISIT_API AMIPuzzleButton : public AMIPuzzleElementBase
+{
+	GENERATED_BODY()
+
+public:
+	AMIPuzzleButton();
+
+	UFUNCTION(BlueprintCallable, Category = "Button")
+	void Press();
+
+	virtual void ReceiveEvent_Implementation(FName EventName) override;
+
+	UFUNCTION(BlueprintPure, Category = "Button")
+	bool IsActive() const { return bIsActive; }
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Button")
+	float ActiveDuration = 5.0f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Button")
+	bool bIsActive = false;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Lever", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+	FTimerHandle ButtonTimerHandle;
+
+	void Expire();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Button")
+	void OnPressed();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Button")
+	void OnExpired();
+};
