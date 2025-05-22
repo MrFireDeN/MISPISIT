1bac98eb356cc87753c357d786fed977414090f9
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-22 16:42:25 +0300

    feat: создан сундук для медиатора

---
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleChest.cpp
@@ -0,0 +1,48 @@
+﻿// MIPuzzleChest.cpp
+
+
+#include "Game/DesignPatterns/Behavioral/Mediator/MIPuzzleChest.h"
+
+
+AMIPuzzleChest::AMIPuzzleChest()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	ID = FName("Chest");
+}
+
+void AMIPuzzleChest::Open()
+{
+	if (!bIsOpen)
+	{
+		bIsOpen = true;
+		OnOpen();
+		Execute_NotifyMediator(this);
+	}
+}
+
+void AMIPuzzleChest::Close()
+{
+	if (bIsOpen)
+	{
+		bIsOpen = false;
+		OnClose();
+		Execute_NotifyMediator(this);
+	}
+}
+
+void AMIPuzzleChest::ReceiveEvent_Implementation(FName EventName)
+{
+	if (EventName == "Open")
+	{
+		Open();
+	}
+
+	if (EventName == "Close")
+	{
+		Close();
+	}
+}
+
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleChest.h
@@ -0,0 +1,37 @@
+﻿// MIPuzzleChest.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MIPuzzleElementBase.h"
+#include "MIPuzzleChest.generated.h"
+
+UCLASS()
+class MISPISIT_API AMIPuzzleChest : public AMIPuzzleElementBase
+{
+	GENERATED_BODY()
+
+public:
+	AMIPuzzleChest();
+
+	UFUNCTION(BlueprintCallable, Category = "Chest")
+	void Open();
+
+	UFUNCTION(BlueprintCallable, Category = "Chest")
+	void Close();
+
+	virtual void ReceiveEvent_Implementation(FName EventName) override;
+
+	UFUNCTION(BlueprintPure, Category = "Chest")
+	bool IsOpen() const { return bIsOpen; }
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Chest")
+	bool bIsOpen;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnOpen();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnClose();
+};
