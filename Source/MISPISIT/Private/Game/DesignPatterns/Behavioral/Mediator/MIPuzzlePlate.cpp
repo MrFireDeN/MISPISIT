098cf1ed3bed1454fae6049e5258f18c11e1b442
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-22 15:43:08 +0300

    feat: создана плита для медиатора

---
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Mediator/MIPuzzlePlate.cpp
@@ -0,0 +1,47 @@
+﻿// MIPuzzlePlate.cpp
+
+
+#include "Game/DesignPatterns/Behavioral/Mediator/MIPuzzlePlate.h"
+
+#include "Components/BoxComponent.h"
+
+
+AMIPuzzlePlate::AMIPuzzlePlate()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+	
+	TriggerZone = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerZone"));
+	TriggerZone->SetupAttachment(RootComponent);
+
+	ID = "Plate";
+}
+
+void AMIPuzzlePlate::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	if (!bIsPressured)
+	{
+		bIsPressured = true;
+		OnPressured();
+		Execute_NotifyMediator(this);
+	}
+}
+
+void AMIPuzzlePlate::NotifyActorEndOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorEndOverlap(OtherActor);
+
+	if (bIsPressured)
+	{
+		bIsPressured = false;
+		OnUnpressured();
+		Execute_NotifyMediator(this);
+	}
+}
+
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Mediator/MIPuzzlePlate.h
@@ -0,0 +1,40 @@
+﻿// MIPuzzlePlate.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MIPuzzleElementBase.h"
+#include "MIPuzzlePlate.generated.h"
+
+class UStaticMeshComponent;
+class UBoxComponent;
+
+UCLASS()
+class MISPISIT_API AMIPuzzlePlate : public AMIPuzzleElementBase
+{
+	GENERATED_BODY()
+
+public:
+	AMIPuzzlePlate();
+
+	UFUNCTION(BlueprintPure, Category = "Plate")
+	bool IsPressured() const { return bIsPressured; }
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Plate", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent> TriggerZone;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Plate", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> Mesh;
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	virtual void NotifyActorEndOverlap(AActor* OtherActor) override;
+
+	bool bIsPressured = false;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Plate")
+	void OnPressured();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Plate")
+	void OnUnpressured();
+};
