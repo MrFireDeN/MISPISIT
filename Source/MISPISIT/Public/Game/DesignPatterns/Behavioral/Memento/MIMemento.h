302025cac723a73ab1907f03541b189a2baa4bb0
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-22 21:32:04 +0300

    feat: созданы интерфейсы для Снимка

---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Memento/MIMemento.h
@@ -0,0 +1,25 @@
+﻿// MIMemento.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MIMemento.generated.h"
+
+UINTERFACE()
+class UMIMemento : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MISPISIT_API IMIMemento
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Memento")
+	void Restore();
+};
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Memento/MIOriginator.h
@@ -0,0 +1,27 @@
+﻿// MIOriginator.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MIOriginator.generated.h"
+
+class IMIMemento;
+
+UINTERFACE()
+class UMIOriginator : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MISPISIT_API IMIOriginator
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Memento")
+	TScriptInterface<IMIMemento> Save();
+};
