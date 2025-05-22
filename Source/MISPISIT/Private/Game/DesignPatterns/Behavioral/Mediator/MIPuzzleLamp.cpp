d587067d4f8c65d3b29070d9c2078a5cac31a0dc
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-22 16:12:28 +0300

    feat: создана лампа для медиатора

---
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleLamp.cpp
@@ -0,0 +1,27 @@
+﻿// MIPuzzleLamp.cpp
+
+
+#include "Game/DesignPatterns/Behavioral/Mediator/MIPuzzleLamp.h"
+
+
+AMIPuzzleLamp::AMIPuzzleLamp()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	ID = FName("Lamp");
+}
+
+void AMIPuzzleLamp::ReceiveEvent_Implementation(FName EventName)
+{
+	if (EventName == "TurnOn")
+	{
+		bIsOn = true;
+		OnTurnedOn();
+	}
+
+	else if (EventName == "TurnOff")
+	{
+		bIsOn = false;
+		OnTurnedOff();
+	}
+}
---
+++ Source/MISPISIT/Public/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleLamp.h
@@ -0,0 +1,28 @@
+﻿// MIPuzzleLamp.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MIPuzzleElementBase.h"
+#include "MIPuzzleLamp.generated.h"
+
+UCLASS()
+class MISPISIT_API AMIPuzzleLamp : public AMIPuzzleElementBase
+{
+	GENERATED_BODY()
+
+public:
+	AMIPuzzleLamp();
+
+	virtual void ReceiveEvent_Implementation(FName EventName) override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Lamp")
+	bool bIsOn = false;
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Lamp")
+	void OnTurnedOn();
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Lamp")
+	void OnTurnedOff();
+};
