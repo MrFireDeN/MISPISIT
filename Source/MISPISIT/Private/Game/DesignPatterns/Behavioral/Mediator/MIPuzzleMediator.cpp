f13cc31782adc967aa10d3ff5e0577812c7aa294
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2025-05-22 17:22:26 +0300

    feat: реализована головоломка

--- Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleMediator.cpp
+++ Source/MISPISIT/Private/Game/DesignPatterns/Behavioral/Mediator/MIPuzzleMediator.cpp
@@ -16,6 +16,28 @@ void UMIPuzzleMediator::RegisterElement(FName ElementID, const TScriptInterface<
 void UMIPuzzleMediator::NotifyFrom(FName Name, const bool State)
 {
 	UE_LOG(LogTemp, Display, TEXT("[%s]::NotifyFrom: %s"), *GetNameSafe(this), *Name.ToString());
+
+	if (Name == "Lever")
+	{
+		if (State)
+		{
+			SendEventTo("Lamp", FName("TurnOn"));
+		}
+		else
+		{
+			SendEventTo("Lamp", FName("TurnOff"));
+		}
+	}
+
+	if (Name == "Button")
+	{
+		bButtonPressed = State;
+	}
+
+	if (Name == "Plate" && bButtonPressed)
+	{
+		SendEventTo("Chest", FName("Open"));
+	}
 }
 
 void UMIPuzzleMediator::SendEventTo(FName ElementID, FName EventName)
